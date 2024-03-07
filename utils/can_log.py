0d86db33de05af319a51aad7aced7c306bb2cfe3
Aryan <aryankashem@gmail.com>
2024-03-07 14:38:06 -0500

    formatting for table

--- utils/can_log.py
+++ utils/can_log.py
@@ -1,71 +1,78 @@
-"""
-This module logs CAN messages and decodes them using DBC files (exported from MSXV).
-"""
+'''
+This module logs CAN messages and decodes them using DBC files (exported from MSXV)
+'''
 
-from tabulate import tabulate
 import os
 import time
 import subprocess
 import cantools
 import can
 
-# Set up CAN interface
 can_bus = can.interface.Bus('can0', bustype='socketcan')
-
-# Configure CAN interface settings
-subprocess.run(['sudo', 'ip', 'link', 'set', 'can0', 'up', 'type', 'can', 'bitrate', '500000'])
+subprocess.run(
+    ['sudo', 'ip', 'link', 'set', 'can0', 'up', 'type', 'can', 'bitrate', '500000']
+)
 time.sleep(1)
-
 # Try again
-subprocess.run(['sudo', 'ip', 'link', 'set', 'can0', 'up', 'type', 'can', 'bitrate', '500000'])
-subprocess.run(['sudo', 'ip', 'link', 'set', 'can0', 'up'], check=True)
+subprocess.run(
+    ['sudo', 'ip', 'link', 'set', 'can0', 'up', 'type', 'can', 'bitrate', '500000']
+)
+subprocess.run(
+    ['sudo', 'ip', 'link', 'set', 'can0', 'up'], check=True
+)
 
-# Header for CAN decoded data table
-CAN_DECODED_DATA_HEADER = [
-    'TIME',
-    'CAN STATE',
-    'CAN ID (Hex)',
-    'Name',
-    'Sender',
-    'Decoded',
-    'Bytes'
-]
-
-print(tabulate([CAN_DECODED_DATA_HEADER], tablefmt="plain"))
+CAN_DECODED_DATA = (
+    'TIME                    '
+    + '  |  '
+    + '[CAN STATE]'
+    + '  |  '
+    + 'CAN ID (Hex)  '
+    + '   '
+    + '  |  '
+    + 'DATA'
+)
+print(CAN_DECODED_DATA + '\n')
 
 try:
     db = cantools.database.load_file("dbc/system_can.dbc")
-except FileNotFoundError:
-    print("DBC file not found. Generate DBC file first.")
+except BaseException:
+    print("Must generate DBC file first")
     print("Ensure that you have specified the path of the DBC file in .env")
 
 while True:
     try:
-        # Receive CAN message
         msg = can_bus.recv()
-
-        # Decode CAN message
         decoded = db.decode_message(msg.arbitration_id, msg.data)
         name = db.get_message_by_frame_id(msg.arbitration_id).name
         sender = db.get_message_by_frame_id(msg.arbitration_id).senders[0]
 
-        # Format and print decoded data using tabulate
-        data_row = [
-            time.asctime(time.localtime()),
-            '[CAN WORKS]',
-            f'CAN ID (Hex): {str(hex(msg.arbitration_id)[2:].zfill(3))}',
-            name,
-            sender,
-            str(decoded),
-            ' '.join(hex(byte)[2:].zfill(2) for byte in msg.data)
-        ]
+        CAN_DECODED_DATA = (
+            time.asctime(time.localtime())
+            + '  |  '
+            + '[CAN WORKS]'
+            + '  |  '
+            + 'CAN ID (Hex): '
+            + str(hex(msg.arbitration_id)[2:].zfill(3))
+            + '  |  '
+            + 'Msg Name: '
+            + name
+            + '  |  '
+            + 'Msg Sender: '
+            + sender
+            + '  |  '
+            + str(decoded)
+            + '  |  '
+            + (' '.join(hex(byte)[2:].zfill(2) for byte in msg.data))
+        )
 
     except Exception as error:
-        # Handle CAN decoding error
-        data_row = [
-            time.asctime(time.localtime()),
-            '[CAN ERROR]',
-            f'CAN ID (Hex): {hex(error.args[0])[2:].zfill(3)}'
-        ]
+        CAN_DECODED_DATA = (
+            time.asctime(time.localtime())
+            + '  |  '
+            + '[CAN ERROR]'
+            + '  |  '
+            + 'CAN ID (Hex): '
+            + hex(error.args[0])[2:].zfill(3)
+        )
     finally:
-        print(tabulate([data_row], tablefmt="plain"))
+        print(CAN_DECODED_DATA)
