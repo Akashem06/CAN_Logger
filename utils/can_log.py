087486539b7d051dd49a497d8b05b233812ce356
Aryan Kashem <aryankashem@gmail.com>
2024-03-08 23:58:57 -0500

    using file writing instead to ensure consistent file updating

--- utils/can_log.py
+++ utils/can_log.py
@@ -4,12 +4,28 @@ import cantools
 import can
 import signal
 import sys
+import os
+
+
+CAN_LOG_FILE = "TODO"
 
 def shutdown_handler(signum, frame):
     print("Shutting down gracefully...")
+    save_data_to_file()
     sys.exit(0)
 
+def save_data_to_file():
+    with open(CAN_LOG_FILE, "a") as log_file:
+        log_file.write(CAN_DECODED_DATA + '\n')
+
+def create_log_file():
+    if not os.path.isfile(CAN_LOG_FILE):
+        with open(CAN_LOG_FILE, "w") as log_file:
+            log_file.write("Log file for CAN decoded data\n")
+
 signal.signal(signal.SIGTERM, shutdown_handler)
+
+create_log_file()
 
 time.sleep(1)
 can_bus = can.interface.Bus('can0', bustype='socketcan')
@@ -65,6 +81,8 @@ while True:
             + (' '.join(hex(byte)[2:].zfill(2) for byte in msg.data))
         )
 
+        save_data_to_file()
+
     except Exception as error:
         CAN_DECODED_DATA = (
             time.asctime(time.localtime())
@@ -74,5 +92,7 @@ while True:
             + 'CAN ID (Hex): '
             + hex(error.args[0])[2:].zfill(3)
         )
+        save_data_to_file()
+
     finally:
         print(CAN_DECODED_DATA)
