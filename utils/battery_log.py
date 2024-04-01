e11c13b6c404ea330e8faa15d7c9e5976095cd81
Aryan Kashem <aryankashem@gmail.com>
2024-03-31 21:37:01 -0400

    battery logging added

---
+++ utils/battery_log.py
@@ -0,0 +1,103 @@
+'''
+This module autogenerates log files and writes Battery stats to them
+'''
+import time
+import signal
+import subprocess
+import sys
+import os
+from datetime import datetime
+import can
+
+formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+BATTERY_LOG_FILE = f"/home/midnightsun/Documents/Battery/{formatted_datetime}.log"
+
+COUNTER = 0
+BATTERY_STATUS_ID = 0x001
+BATTERY_VT_ID = 0x001 # needs to be updated
+
+filters = [
+  { "can_id": 0x001, "can_mask": 0xFFF, "extended": False }, # BATTERY STATUS ID
+  { "can_id": 0x001, "can_mask": 0xFFF, "extended": False } # CURRENT SENSE DATA
+]
+
+def shutdown_hadnler(signum, frame):
+  '''
+  Shutdown handler when Rpi shutoff
+  '''
+  print("Shutting down gracefully...")
+  save_data_to_file()
+  sys.exit(0)
+
+def save_data_to_file():
+  '''
+  Writes data to the file
+  '''
+  with open(BATTERY_LOG_FILE, "a") as log_file:
+    log_file.write(BATTERY_STATS + '\n')
+
+def create_log_file():
+  '''
+  Autogenerates the file if not created already, then writes to it
+  '''
+  if not os.path.isfile(BATTERY_LOG_FILE):
+    with open(BATTERY_LOG_FILE, "w") as log_file:
+      log_file.write("Log file for Battery status\n")
+
+def unpack_message(msg):
+  '''
+  Unpacks the CAN message into readable data
+  '''
+  if msg.arbitration_id == BATTERY_STATUS_ID:
+    fault = int.from_bytes(msg.data[0:2])
+    fan1_status = int.from_bytes(msg.data[2])
+    fan1_status = int.from_bytes(msg.data[3])
+  elif msg.arbitration_id == BATTERY_VT_ID:
+    voltage = int.from_bytes(msg.data[0:2])
+    current = int.from_bytes(msg.data[2:4])
+    temperature = int.from_bytes(msg.data[4:6])
+    batt_soc = int.from_bytes(msg.data[6:8])
+  else:
+    print("ERROR")
+
+signal.signal(signal.SIGTERM, shutdown_hadnler)
+
+create_log_file()
+subprocess.run(['sudo', 'ip', 'link', 'set', 'can0', 'up', 'type', 'can', 'bitrate', '500000'])
+time.sleep(1)
+subprocess.run(['sudo', 'ip', 'link', 'set', 'can0', 'up'])
+can_bus = can.interface.Bus(channel='can0', bustype='socketcan', can_filters=filters)
+
+BATTERY_STATS = (
+  'TIME                    '
+  + '  |  '
+  + 'MAX CELL VOLTAGE'
+  + '  |  '
+  + 'MIN CELL VOLTAGE'
+  + '  |  '
+  + 'CURRENT (mA)'
+  + '  |  '
+  + 'TEMPERATURE'
+  + '  |  '
+  + 'FAN 1'
+  + '  |  '
+  + 'FAN 2'
+)
+save_data_to_file()
+
+while True:
+  COUNTER = (COUNTER + 1) % 61
+  if not COUNTER%60:
+    subprocess.run(['cat', BATTERY_LOG_FILE])
+
+  try:
+    msg = can_bus.recv()
+    unpack_message(msg)
+    save_data_to_file()
+
+  except Exception as error:
+    BATTERY_STATS = (
+      time.asctime(time.localtime())
+      + '[LOGGING ERROR]'
+    )
+    save_data_to_file()
--- utils/can_log.py
+++ utils/can_log.py
@@ -11,31 +11,31 @@ import cantools
 import can
 
 formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-CAN_LOG_FILE = f"/home/midnightsun/Documents/{formatted_datetime}.log"
+CAN_LOG_FILE = f"/home/midnightsun/Documents/CAN/{formatted_datetime}.log"
 COUNTER = 0
 
 def shutdown_handler(signum, frame):
-    '''
-    Shutdown handler when Rpi shutoff
-    '''
-    print("Shutting down gracefully...")
-    save_data_to_file()
-    sys.exit(0)
+  '''
+  Shutdown handler when Rpi shutoff
+  '''
+  print("Shutting down gracefully...")
+  save_data_to_file()
+  sys.exit(0)
 
 def save_data_to_file():
-    '''
-    Writes data to the file
-    '''
-    with open(CAN_LOG_FILE, "a") as log_file:
-        log_file.write(CAN_DECODED_DATA + '\n')
+  '''
+  Writes data to the file
+  '''
+  with open(CAN_LOG_FILE, "a") as log_file:
+    log_file.write(CAN_DECODED_DATA + '\n')
 
 def create_log_file():
-    '''
-    Autogenerates the file if not created already, then writes to it
-    '''
-    if not os.path.isfile(CAN_LOG_FILE):
-        with open(CAN_LOG_FILE, "w") as log_file:
-            log_file.write("Log file for CAN decoded data\n")
+  '''
+  Autogenerates the file if not created already, then writes to it
+  '''
+  if not os.path.isfile(CAN_LOG_FILE):
+    with open(CAN_LOG_FILE, "w") as log_file:
+      log_file.write("Log file for CAN decoded data\n")
 
 signal.signal(signal.SIGTERM, shutdown_handler)
 
@@ -47,61 +47,58 @@ subprocess.run(['sudo', 'ip', 'link', 'set', 'can0', 'up'])
 can_bus = can.interface.Bus(channel='can0', bustype='socketcan')
 
 CAN_DECODED_DATA = (
-    'TIME                    '
-    + '  |  '
-    + '[CAN STATE]'
-    + '  |  '
-    + 'CAN ID (Hex)  '
-    + '   '
-    + '  |  '
-    + 'DATA'
+  'TIME                    '
+  + '  |  '
+  + '[CAN STATE]'
+  + '  |  '
+  + 'CAN ID (Hex)  '
+  + '   '
+  + '  |  '
+  + 'DATA'
 )
 save_data_to_file()
 
 try:
-    db = cantools.database.load_file("/home/midnightsun/Downloads/telemMSXV/dbc/system_can.dbc")
+  db = cantools.database.load_file("/home/midnightsun/Downloads/telemMSXV/dbc/system_can.dbc")
 except BaseException:
-    print("Must generate DBC file first")
-    print("Ensure that you have specified the path of the DBC file in .env")
+  print("Must generate DBC file first")
+  print("Ensure that you have specified the path of the DBC file in .env")
 
 while True:
-    COUNTER += 1
-    if not COUNTER%60:
-        subprocess.run(['cat', CAN_LOG_FILE])
-    try:
-        msg = can_bus.recv()
-        decoded = db.decode_message(msg.arbitration_id, msg.data)
-        name = db.get_message_by_frame_id(msg.arbitration_id).name
-        sender = db.get_message_by_frame_id(msg.arbitration_id).senders[0]
-
-        CAN_DECODED_DATA = (
-            time.asctime(time.localtime())
-            + '  |  '
-            + '[CAN WORKS]'
-            + '  |  '
-            + 'CAN ID (Hex): '
-            + str(hex(msg.arbitration_id)[2:].zfill(3))
-            + '  |  '
-            + 'Msg Name: '
-            + name
-            + '  |  '
-            + 'Msg Sender: '
-            + sender
-            + '  |  '
-            + str(decoded)
-            + '  |  '
-            + (' '.join(hex(byte)[2:].zfill(2) for byte in msg.data))
-        )
-
-        save_data_to_file()
-
-    except Exception as error:
-        CAN_DECODED_DATA = (
-            time.asctime(time.localtime())
-            + '  |  '
-            + '[CAN ERROR]'
-            + '  |  '
-            + 'CAN ID (Hex): '
-            + hex(error.args[0])[2:].zfill(3)
-        )
-        save_data_to_file()
+  COUNTER = (COUNTER + 1) % 61
+  if not COUNTER%60:
+    subprocess.run(['cat', CAN_LOG_FILE])
+  try:
+    msg = can_bus.recv()
+    decoded = db.decode_message(msg.arbitration_id, msg.data)
+    name = db.get_message_by_frame_id(msg.arbitration_id).name
+    sender = db.get_message_by_frame_id(msg.arbitration_id).senders[0]
+    CAN_DECODED_DATA = (
+      time.asctime(time.localtime())
+      + '  |  '
+      + '[CAN WORKS]'
+      + '  |  '
+      + 'CAN ID (Hex): '
+      + str(hex(msg.arbitration_id)[2:].zfill(3))
+      + '  |  '
+      + 'Msg Name: '
+      + name
+      + '  |  '
+      + 'Msg Sender: '
+      + sender
+      + '  |  '
+      + str(decoded)
+      + '  |  '
+      + (' '.join(hex(byte)[2:].zfill(2) for byte in msg.data))
+    )
+    save_data_to_file()
+  except Exception as error:
+    CAN_DECODED_DATA = (
+      time.asctime(time.localtime())
+      + '  |  '
+      + '[CAN ERROR]'
+      + '  |  '
+      + 'CAN ID (Hex): '
+      + hex(error.args[0])[2:].zfill(3)
+    )
+    save_data_to_file()
